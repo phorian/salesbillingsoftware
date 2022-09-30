8619249890435690b22704ead4c45f4b1897a3cf
drene <jinadopeyemi@gmail.com>
2022-09-30 02:13:33 +0100

    fixed simple error

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include<stdio.h>
+#include <time.h>
+
+int main() {
+
+    int opt;
+    
+    printf("X-Nova Tech");
+    printf("\nPlease select your operation");
+    printf("\n\n1.Generate Invoice");
+    printf("\n2.Show all Invoices");
+    printf("\n3.Search Invoice");
+    printf("\n4.Exit");
+    printf("\n\nPlease Enter Your Choice: ");
+    scanf("%d",&opt);
+    return 0;
+}
