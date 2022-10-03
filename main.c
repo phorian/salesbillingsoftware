f93f763c025217e11f4091e7f7202acaf61c693f
drene <jinadopeyemi@gmail.com>
2022-10-03 22:45:02 +0100

    More functionalities

--- main.c
+++ main.c
@@ -20,12 +20,57 @@ struct orders
     int numOfItems;
     struct items itm[50];
 };
+void generateBillHeader;
+void generateBillBody;
+void generateBillFooter;
 
+
+
+void generateBillHeader(char name[50], char date[30]){
+    printf("\n\n");
+    printf("\t     X-Nova Tech");
+    printf("\n\t   --------------");
+    printf("\nDate:%s", date);
+    printf("\nInvoice To: %s", name);
+    printf("\n");
+    printf("---------------------------");
+    printf("Items\t\t");
+    printf("Qty\t\t");
+    printf("Total\t\t");
+    printf("\n-----------------------------");
+    printf("\n\n");
+}
+
+void generateBillBody(char itemName[20], int qty, float price){
+    printf("%s\t\t", itemName);
+    printf("%d\t\t", qty);
+    printf("%.2f\t\t", qty * price);
+    printf("\n");
+}
+
+void generateBillFooter(float total){
+    printf("\n");
+    float dis = 0.1*total;
+    float netTotal = total-dis;
+    float cgst = 0.09*netTotal,grandTotal = netTotal + 2;
+    printf("-------------------------------------------\t");
+    printf("Sub Total\t\t\t%.2f",total);
+    printf("\nDiscount @10%s\t\t\t%.2f","%"dis);
+    printf("\n\t\t\t\t--------");
+    printf("Net Total\t\t\t\t%.2f", netTotal);
+    printf("CGST @9%S\t\t\t\t%.2f", cgst);
+    printf("SGST @9%s\t\t\t\t%.2f", cgst);
+    printf("\n---------------------------------------------");
+    printf("\nGrand Total\t\t\t%.2f",grandTotal);
+    printf("\n---------------------------------------------");
+
+}
 
 int main() {
 
     int opt,n;
     struct orders ord;
+    float total;
 
     printf("===============X-Nova Tech===================");
     printf("\nPlease select your operation");
@@ -40,25 +85,34 @@ int main() {
     switch (opt)
     {
     case 1:
-        printf("\nPlease Enter name of customer: ");
+        printf("\nPlease Enter name of customer:\t");
         fgets(ord.customerName,50,stdin);
         ord.customerName[strlen(ord.customerName)- 1]=0;
         strcpy(ord.date, __DATE__);
         printf("Please enter the number of items:\t");
         scanf("%d",&n);
+        ord.numOfItems = n;
 
+        //to iterate over the number of items purchased
         for (int i = 0; i < n; i++)
         {
             /* code */
             fgetc(stdin);
-            printf("Please enter the item %d",i+1);
+            printf("Please enter the item %d: \t",i+1);
             fgets(ord.itm[i].itemName,20,stdin);
             ord.itm[i].itemName[strlen(ord.itm[i].itemName)-1]=0;
             printf("\nPlease enter the quantity:\t");
             scanf("%d",&ord.itm[i].qty);
-            printf("\nPlease enter the unit price: \t");
+            printf("\nPlease enter the unit price:\t");
             scanf("%f", &ord.itm[i].price);
+            total += ord.tm[i].qty * ord.itm[i].price;
         }
+
+        generateBillHeader(ord.customerName, ord.date);
+        for(int i = 0; i < ord.numOfItems;i++){
+            generateBillBody(ord.itm[i].itemName,ord.itm[i].qty, ord.itm[i].price);
+        }
+        generateBillFooter(total);
         
         break;
     
